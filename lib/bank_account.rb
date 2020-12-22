1334ab2621597e2036ae2470e833862dd707e300
reyes2981 <noe.reyes92@yahoo.com>
2020-12-21 21:30:26 -0600

    commit tree 7be16251d57ed6cbc9a74d815c8a0a0dde355386

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+    attr_reader :name #method is unable to be changed
+    attr_accessor :balance, :status, :account_hash
+
+    def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    end
+
+    def deposit(deposit_amount)
+        @balance += deposit_amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if status == "open" && balance > 0
+            true
+        else
+            false
+        end
+    end
+
+    def close_account
+       @status = "closed"
+    end
 
 end
